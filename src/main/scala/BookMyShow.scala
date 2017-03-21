4565a5b1c463886174392c4692a31875189958b2
prashant <12ps93r@gmail.com>
2017-03-21 11:55:27 +0530

    Commit 3 with BMS

--- src/main/scala/BookMyShow.scala
+++ src/main/scala/BookMyShow.scala
@@ -3,38 +3,38 @@ import akka.routing.FromConfig
 import com.typesafe.config.ConfigFactory
 
 
-class BookMyShowApp extends Actor{
+class BookMyShow extends Actor{
 
   var status=false
 
   override def receive = {
 
-    case bookSeat if(bookSeat.equals("Booking Seat 1"))=>{
+    case bookSeat if(bookSeat.equals("Booking Seat "))=>{
       if(status==false)
         {
           status=true
           println(s"Thanks for $bookSeat !!")
         }
       else {
-        //status=false
-        println("Sorry this seat is already booked !!")
+
+        println("Seat already booked !!")
       }
 
     }
 
-    case cancelSeat if(cancelSeat.equals("Canceling Seat 1"))=>{
+    case cancelSeat if(cancelSeat.equals("Canceling Seat "))=>{
 
       if(status==true) {
         status = false
         println(s"$cancelSeat Successful")
       }
       else
-        println("Already Canceled !!")
+        println("Seat Canceled Already !!")
     }
   }
 }
 
-object BookMyShow extends App{
+object BookMyShowApp extends App{
 
   val config = ConfigFactory.parseString(
     """
@@ -48,16 +48,16 @@ object BookMyShow extends App{
   )
 
   val system = ActorSystem("RouterSystem", config)
-  val router = system.actorOf(FromConfig.props(Props[BookMyShowApp]), "poolRouter")
+  val router = system.actorOf(FromConfig.props(Props[BookMyShow]), "poolRouter")
 
-  router ! "Booking Seat 1"
+  router ! "Booking Seat "
   Thread.sleep(1000)
-  router ! "Booking Seat 1"
+  router ! "Booking Seat "
   Thread.sleep(1000)
-  router ! "Canceling Seat 1"
+  router ! "Canceling Seat "
   Thread.sleep(1000)
-  router ! "Canceling Seat 1"
+  router ! "Canceling Seat "
   Thread.sleep(1000)
-  router ! "Booking Seat 1"
+  router ! "Booking Seat "
 
 }
